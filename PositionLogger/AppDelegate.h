f07c4f59127c666bca8b04aee8eaa2ec7babf8af
Katie Bartel <kwbartel@mit.edu>
2016-04-16 16:10:20 -0400

    Basic position logger app

---
+++ PositionLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDeleg/Users/student/Desktop/Lab_1/PositionLoggerBlank/PositionLogger/AppDelegate.mate.h
+//  PositionLogger
+//
+//  Created by Sam Madden on 2/3/16.
+//  Copyright © 2016 Sam Madden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
