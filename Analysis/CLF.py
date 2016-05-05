7efa6d805da5164360e297696a696ee0689b61a5
Katie Bartel <kwbartel@mit.edu>
2016-05-05 01:24:38 -0400

    Axis-specific CLF features, using original test/train set

--- Analysis/CLF.py
+++ Analysis/CLF.py
@@ -8,44 +8,50 @@ import sklearn.tree as tree
 
 files = ["bench/kwame_bench.csv", "bench/katie_bench1.csv", "curl/kwame_curl.csv", "curl/katie_curl1.csv", "row/kwame_row.csv", "row/katie_row1.csv", "flye/kwame_flye.csv", "flye/katie_flye1.csv"]
 classes = [0,0,1,1,2,2,3,3]
-metrics = []
 
 test_files = ["bench/katie_bench2.csv", "curl/katie_curl2.csv", "row/katie_row2.csv", "flye/katie_flye2.csv"]
 test_classes = [0, 1, 2, 3]
 
 def make_features(csv_files, save_graphs=False):
     idx = 0
-    for file in files:
+    metrics = []
+    for file in csv_files:
         met = []
         print file
 
-        vars = [' RAccelX',' RAccelY',' RAccelZ']
-        df1 = pd.read_csv(file)
-        a = df1[vars]
-        mag = np.linalg.norm(a, axis=1)
+        # Load variables
+        vars = [' RAccelX', ' LAccelX', ' RGyroX', ' LGyroX',
+        ' RAccelY', ' LAccelY', ' RGyroY', ' LGyroY',
+        ' RAccelZ', ' LAccelZ', ' RGyroZ', ' LGyroZ' ]
+        raw_values = pd.read_csv(file)
+        raw_values = raw_values[vars]
 
-        #remove mean from mag
-        mean = np.mean(mag)
-        mag = mag - mean
+        raw_values = np.matrix(raw_values).T
 
-        #plot power spectrum of magnitude
-        numplots = 1#len(vars)+1
-        curplot = 1
+        for i in range(raw_values.shape[0]):
+            # Remove mean from raw row
+            row = raw_values[i]
+            mean = np.mean(row)
+            mag = row - mean
 
-        plt.subplot(3,numplots,curplot+2)
-        (power,freq) = plt.psd(mag, 512, 20,detrend=plt.mlab.detrend_none)
+            #plot power spectrum of magnitude
+            numplots = 1#len(vars)+1
+            curplot = 1
+
+            plt.subplot(3,numplots,curplot+2)
+            (power,freq) = plt.psd(mag, 512, 20,detrend=plt.mlab.detrend_none)
 
 
-        #label power spectrum graph
-        meanpower = np.mean(power)
+            #label power spectrum graph
+            meanpower = np.mean(power)
 
-        # record a bunch of metrics of magnitude
-        met.append(mean)  #mean of accel
-        met.append(plt.mlab.entropy(power, 20)) #entropy of power, binned into 20 bins
-        met.append(meanpower)  #average of power
-        met.append(np.std(power)) #std dev of power
-        met.append(np.max(power))  #maximum power
-        met.append(freq[np.argmax(power)])  #maximum frequency
+            # record a bunch of metrics of magnitude
+            met.append(mean)  #mean of accel
+            #met.append(plt.mlab.entropy(power, 20)) #entropy of power, binned into 20 bins
+            #met.append(meanpower)  #average of power
+            met.append(np.std(power)) #std dev of power
+            #met.append(np.max(power))  #maximum power
+            #met.append(freq[np.argmax(power)])  #maximum frequency
         metrics.append(met)
 
         # Save graphs of features
@@ -67,7 +73,6 @@ def make_features(csv_files, save_graphs=False):
             plt.specgram(mag, 512, 20)
             plt.ylabel('')
 
-
             s = "mag mu = " + ("%.2f" % mean) + "\n"
             s = s + "psd e: " + ("%.2f" % plt.mlab.entropy(power, 20)) + "; mu = " + ("%.2f" % meanpower) + "\n"
             s = s + "psd std: " + ("%.2f" % np.std(power))
@@ -86,28 +91,24 @@ def make_features(csv_files, save_graphs=False):
 
     return metrics
 
+# get features for training set
 train_features = make_features(files)
-
-print ['mag_mean','mag_power_entropy','mag_power_mean', 'mag_power_std', 'mag_power_max', 'mag_power_max_freq']
-for m in metrics:
-    for v in m:
-        print ("%.2f, " % v),
-    print
-print classes
+print len(train_features[0])
 
 #train decision tree classifier on data
 clf = DecisionTreeClassifier(random_state=0,max_depth=3)
-
 clf.fit(train_features, classes)
-print clf
 
+# get test set results
 test_features = make_features(test_files)
-
+print test_classes
 print clf.predict(test_features)
-print classes
-cm = confusion_matrix(test_classes,clf.predict(metrics))
+
+# confusion matrix
+cm = confusion_matrix(test_classes,clf.predict(test_features))
 print cm
 
+# save CLF tree
 with open('graph.dot', 'w') as file:
     tree.export_graphviz(clf, out_file = file)
 
