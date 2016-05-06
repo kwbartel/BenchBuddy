47584e17c90fc18ceb2808ab8578573a59edeb5e
Kwame Efah <kwameefah@dhcp-18-189-108-199.dyn.mit.edu>
2016-05-06 16:32:36 -0400

    readings analyzer template

---
+++ ReadingsAnalyzer.h
@@ -0,0 +1,19 @@
+//
+//  ReadingsAnalyzer.h
+//  WorkoutLogger
+//
+//  Created by Kwame Efah  on 5/6/16.
+//  Copyright © 2016 Sam Madden. All rights reserved.
+//
+
+#ifndef ReadingsAnalyzer_h
+#define ReadingsAnalyzer_h
+
+
+
+@interface ReadingsAnalyzer : NSObject
+
+
+@end
+
+#endif /* ReadingsAnalyzer_h */
---
+++ ReadingsAnalyzer.m
@@ -0,0 +1,15 @@
+//
+//  ReadingsAnalyzer.m
+//  WorkoutLogger
+//
+//  Created by Kwame Efah  on 5/6/16.
+//  Copyright © 2016 Sam Madden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ReadingsAnalyzer.h"
+
+@implementation ReadingsAnalyzer {
+}
+
+@end
